a6f5aeff6523dd00951505c9e9aa37ab931af4c2
Barbara Honold <honold@gmx.de>
2018-09-07 10:07:52 +0100

    Initial commit

---
+++ src/KlasseZumTesten.java
@@ -0,0 +1,10 @@
+
+public class KlasseZumTesten {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mit dieser Klasse teste ich den Umgang mit GitHub.");
+
+	}
+
+}
---
+++ src/ZweiteKlasse.java
@@ -0,0 +1,9 @@
+
+public class ZweiteKlasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Auch hier wird nur getestet.");
+	}
+
+}
