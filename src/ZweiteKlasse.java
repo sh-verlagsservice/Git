a3407f10699bf4ac8a7015ae905b811ad384e739
Barbara Honold <honold@gmx.de>
2018-09-07 10:16:52 +0100

    Erster Commit
    Hier wurde nur eine Zeile geändert, um den ersten Commit zu testen.

--- src/ZweiteKlasse.java
+++ src/ZweiteKlasse.java
@@ -4,6 +4,8 @@ public class ZweiteKlasse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Auch hier wird nur getestet.");
+		System.out.println("Zeile 2");
+		System.out.println("Zeile 3 wird nach dem Hochladen geändert.");
 	}
 
 }
